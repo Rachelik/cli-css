3d1f5582ed305329de26230d9aeb7f48ff666af3
Rachelik <rachel.ik.87@gmail.com>
2020-04-03 00:02:03 +0800

    Change color output but then only can change when convert #ffffff (white color)

--- index.js
+++ index.js
@@ -3,24 +3,36 @@ var nodeDoc = process.argv;
 const convertRgbToHex = require("./rgbtohex.js");
 const convertHexToRgb = require("./hextorgb.js");
 
+
+
 //Part 3 separate your conversion functions out into separate module files
 if (nodeDoc[2] === "hex") {
     var red = convertHexToRgb.hexToRgb(nodeDoc[3]).r;
     var green = convertHexToRgb.hexToRgb(nodeDoc[3]).g;
     var blue = convertHexToRgb.hexToRgb(nodeDoc[3]).b;
-    console.log("rgb( " + red + ", " + green + ", " + blue + " )");
+
+    var conversionInRgb = "rgb( " + red + ", " + green + ", " + blue + " )"
+    console.log(conversionInRgb);
+
+    // [color; font-weight]
+    // 31 red, 32 green, 34 blue, 38 black, 37 grey
+    // 1 is bold, 2 is normal, 3 is italic, 4 underline...
+    console.log(`\x1b[31;1;${red};${green};${blue}m%s\x1b[0m`, conversionInRgb);
 
 } else if (nodeDoc[2] === "rgb") {
     var red = parseInt(nodeDoc[3]);
     var green = parseInt(nodeDoc[4]);
     var blue = parseInt(nodeDoc[5]);
-    console.log(convertRgbToHex.rgbToHex(red, green, blue));
+    var conversionInHex = convertRgbToHex.rgbToHex(red, green, blue);
+
+    console.log(conversionInHex);
+    // set as red
+    var rred = 255;
+    var bblue = 0;
+    var ggreen = 0;
+    console.log(`\x1b[31;1;${red};${green};${blue}m%s\x1b[0m`, conversionInHex);
 };
 
-// // set as red
-// var r = 255;
-// var b = 0;
-// var g = 0;
 
-// // this special console will create colored output
-// console.log(`\x1b[38;2;${r};${g};${b}m%s\x1b[0m`, myOutput);
+
+// this special console will create colored output
