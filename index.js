a54bf55a15bf3cf89d6cf73982791ea4b4b9775d
Rachelik <rachel.ik.87@gmail.com>
2020-04-02 23:02:56 +0800

    Add if else if loop for hex and rgb to trigger which conversion

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-// hex = #0033ff
-PART 1 Convert hex to rgb ****************************
+var nodeDoc = process.argv;
+
+// PART 1 Convert hex to rgb ****************************
 function hexToRgb(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : null;
-}
-
-var red = hexToRgb(process.argv[3]).r;
-var green = hexToRgb(process.argv[3]).g;
-var blue = hexToRgb(process.argv[3]).b;
-
-console.log("rgb( " + red + ", " + green + ", " + blue + " )");
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+};
 
 // PART 2 Convert rgb to hex ****************************
 function componentToHex(c) {
-  var hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
-}
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+};
 
 function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+};
+
+if (nodeDoc[2] === "hex") {
+    var red = hexToRgb(nodeDoc[3]).r;
+    var green = hexToRgb(nodeDoc[3]).g;
+    var blue = hexToRgb(nodeDoc[3]).b;
+    console.log("rgb( " + red + ", " + green + ", " + blue + " )");
+
+} else if (nodeDoc[2] === "rgb") {
+    var red = parseInt(nodeDoc[3]);
+    var green = parseInt(nodeDoc[4]);
+    var blue = parseInt(nodeDoc[5]);
+    console.log(rgbToHex(red, green, blue));
 }
-
-var nodeDoc = process.argv
-
-var red = parseInt(nodeDoc[3]);
-var green = parseInt(nodeDoc[4]);
-var blue = parseInt(nodeDoc[5]);
-
-console.log(rgbToHex(red, green, blue)); // #0033ff
