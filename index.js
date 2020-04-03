9e3b75cc2f1da493297ddfc8fcd17464bb611dc4
Rachelik <rachel.ik.87@gmail.com>
2020-04-03 23:52:09 +0800

    include hex to hsl and rgb to hsl that is missed out

--- index.js
+++ index.js
@@ -1,34 +1,65 @@
 var nodeDoc = process.argv;
 
-const convertRgbToHex = require("./rgbtohex.js");
 const convertHexToRgb = require("./hextorgb.js");
-
-
+const convertHexToHsl = require("./hextohsl.js");
+const convertRgbToHex = require("./rgbtohex.js");
+const convertRgbToHsl = require("./rgbtohsl.js");
 
 //Part 3 separate your conversion functions out into separate module files
-if (nodeDoc[2] === "hex") {
-    var red = convertHexToRgb.hexToRgb(nodeDoc[3]).r;
-    var green = convertHexToRgb.hexToRgb(nodeDoc[3]).g;
-    var blue = convertHexToRgb.hexToRgb(nodeDoc[3]).b;
+    if (nodeDoc[2] === "hex") {
+        var red = convertHexToRgb.hexToRgb(nodeDoc[3]).r;
+        var green = convertHexToRgb.hexToRgb(nodeDoc[3]).g;
+        var blue = convertHexToRgb.hexToRgb(nodeDoc[3]).b;
+        var colorInRgb = "rgb( " + red + ", " + green + ", " + blue + " )"
+        console.log(colorInRgb);
 
-    var conversionInRgb = "rgb( " + red + ", " + green + ", " + blue + " )"
-    console.log(conversionInRgb);
+        var colorInHsl = convertHexToHsl.hexToHsl(nodeDoc[3]);
+        console.log(colorInHsl);
 
-    // [color; font-weight]
-    // 31 red, 32 green, 34 blue, 38 black, 37 grey
-    // 1 is bold, 2 is normal, 3 is italic, 4 underline...
-    console.log(`\x1b[31;1;${red};${green};${blue}m%s\x1b[0m`, conversionInRgb);
 
-} else if (nodeDoc[2] === "rgb") {
-    var red = parseInt(nodeDoc[3]);
-    var green = parseInt(nodeDoc[4]);
-    var blue = parseInt(nodeDoc[5]);
-    var conversionInHex = convertRgbToHex.rgbToHex(red, green, blue);
+        // [color; font-weight]
+        // 31 red, 32 green, 34 blue, 38 black, 37 grey
+        // 1 is bold, 2 is normal, 3 is italic, 4 underline...
+        console.log(`\x1b[31m%s\x1b[0m`, colorInRgb);
 
-    console.log(conversionInHex);
-    // set as red
-    var rred = 255;
-    var bblue = 0;
-    var ggreen = 0;
-    console.log(`\x1b[31;1;${red};${green};${blue}m%s\x1b[0m`, conversionInHex);
-};
+    } else if (nodeDoc[2] === "rgb") {
+        var red = parseInt(nodeDoc[3]);
+        var green = parseInt(nodeDoc[4]);
+        var blue = parseInt(nodeDoc[5]);
+        var colorInHex = convertRgbToHex.rgbToHex(red, green, blue);
+        console.log(`\x1b[31m%s\x1b[0m`, colorInHex);
+
+        var colorInHSL = convertRgbToHsl.rgbToHsl(red, green, blue);
+        console.log(colorInHSL);
+    };
+
+// var colors = require('./colors.txt');
+
+// var func = function(data) {
+//     console.log('Line: ' + data);
+// };
+
+// var readlines = function(input, func) {
+//     var remaining = '';
+
+//     input.on('data', function(data) {
+//         remaining += data;
+
+//         var index = remaining.indexOf('\n');
+//         while (index > -1) {
+//             var line = remaining.substring(0, index);
+//             remaining = remaining.substring(index + 1);
+//             func(line);
+//             index = remaining.indexOf('\n');
+//         };
+//     });
+
+//     input.on('end', function() {
+//         if (remaining.length > 0) {
+//             func(remaining);
+//         };
+//     });
+// };
+
+// var input = colors.createReadStream('colors.txt');
+// var dataHex = readLines(input, func);
---
+++ rgbtohsl.js
@@ -0,0 +1,28 @@
+function rgbToHsl(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        };
+        h /= 6;
+    };
+
+    s = s*100;
+    s = Math.round(s);
+    l = l*100;
+    l = Math.round(l);
+    h = Math.round(360*h);
+
+    return 'hsl( ' + h + ', ' + s + '%, ' + l + '% )';
+};
+
+module.exports.rgbToHsl = rgbToHsl;
