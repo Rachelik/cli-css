f71fb879c8b043af6e67e4f70e075dff14966cb9
Rachelik <rachel.ik.87@gmail.com>
2020-04-04 18:47:15 +0800

    add script that is able to convert hex from files to display rgb and hsl

--- index.js
+++ index.js
@@ -1,65 +1,38 @@
 var nodeDoc = process.argv;
 
+var fs = require('fs');
+
 const convertHexToRgb = require("./hextorgb.js");
 const convertHexToHsl = require("./hextohsl.js");
 const convertRgbToHex = require("./rgbtohex.js");
 const convertRgbToHsl = require("./rgbtohsl.js");
 
-//Part 3 separate your conversion functions out into separate module files
-    if (nodeDoc[2] === "hex") {
-        var red = convertHexToRgb.hexToRgb(nodeDoc[3]).r;
-        var green = convertHexToRgb.hexToRgb(nodeDoc[3]).g;
-        var blue = convertHexToRgb.hexToRgb(nodeDoc[3]).b;
-        var colorInRgb = "rgb( " + red + ", " + green + ", " + blue + " )"
-        console.log(colorInRgb);
+//Part 3 separate your conversion functions out into separate module files (convert to HSL as well)
+//Part 4 able to  take in a number of colors that is, to convert colors.txt file too
+if (nodeDoc[2] === "hex") {
+    for(var i = 3; i < nodeDoc.length; i++) {
+        // console.log(nodeDoc[i]);
 
-        var colorInHsl = convertHexToHsl.hexToHsl(nodeDoc[3]);
-        console.log(colorInHsl);
+        var red = convertHexToRgb.hexToRgb(nodeDoc[i]).r;
+        var green = convertHexToRgb.hexToRgb(nodeDoc[i]).g;
+        var blue = convertHexToRgb.hexToRgb(nodeDoc[i]).b;
 
+        //convert hex to rgb
+        var colorInRgb = "rgb( " + red + ", " + green + ", " + blue + " )";
 
-        // [color; font-weight]
-        // 31 red, 32 green, 34 blue, 38 black, 37 grey
-        // 1 is bold, 2 is normal, 3 is italic, 4 underline...
+        //convert hex to hsl
+        var colorInHsl = convertHexToHsl.hexToHsl(nodeDoc[i]);
+
+        console.log(nodeDoc[i]);
         console.log(`\x1b[31m%s\x1b[0m`, colorInRgb);
-
-    } else if (nodeDoc[2] === "rgb") {
-        var red = parseInt(nodeDoc[3]);
-        var green = parseInt(nodeDoc[4]);
-        var blue = parseInt(nodeDoc[5]);
-        var colorInHex = convertRgbToHex.rgbToHex(red, green, blue);
-        console.log(`\x1b[31m%s\x1b[0m`, colorInHex);
-
-        var colorInHSL = convertRgbToHsl.rgbToHsl(red, green, blue);
-        console.log(colorInHSL);
+        console.log(`\x1b[35m%s\x1b[0m`, colorInHsl);
     };
-
-// var colors = require('./colors.txt');
-
-// var func = function(data) {
-//     console.log('Line: ' + data);
-// };
-
-// var readlines = function(input, func) {
-//     var remaining = '';
-
-//     input.on('data', function(data) {
-//         remaining += data;
-
-//         var index = remaining.indexOf('\n');
-//         while (index > -1) {
-//             var line = remaining.substring(0, index);
-//             remaining = remaining.substring(index + 1);
-//             func(line);
-//             index = remaining.indexOf('\n');
-//         };
-//     });
-
-//     input.on('end', function() {
-//         if (remaining.length > 0) {
-//             func(remaining);
-//         };
-//     });
-// };
-
-// var input = colors.createReadStream('colors.txt');
-// var dataHex = readLines(input, func);
+} else if (nodeDoc[2] === "rgb") {
+    var red = parseInt(nodeDoc[3]);
+    var green = parseInt(nodeDoc[4]);
+    var blue = parseInt(nodeDoc[5]);
+    var colorInHex = convertRgbToHex.rgbToHex(red, green, blue);
+    var colorInHsl = convertRgbToHsl.rgbToHsl(red, green, blue);
+    console.log(`\x1b[31m%s\x1b[0m`, colorInHex);
+    console.log(`\x1b[35m%s\x1b[0m`, colorInHsl);
+};
